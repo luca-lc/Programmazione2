3138ddc325bfa12f0148eb0ef1b411f5c017b8b3
Luca <luca.lucone.canessa@gmail.com>
2017-01-13 19:09:43 +0100

    Trattamento program

--- Progetto2/Progetto2.ml
+++ Progetto2/Progetto2.ml
@@ -1,13 +1,34 @@
+(*
+--- Type definitions ------
+*)
 type exp =  Int of int;;
+type defin = Funz of string * string * exp;;
+type prog = Program of (defin list) * exp;;
+
+
+
+(*
+--- Function definitions ------
+*)
+exception EmptyEnv;;
+let fenv0 = fun x -> raise EmptyEnv;;
+let env0 = fenv0;;
+let ext env e v = fun y -> if e = y then v else env y;;
+
+
 
 let rec eval (e:exp) env fenv = match e with
                     Int n -> n;;
-exception EmptyEnv;;
-let fenv0 = fun x -> raise EmptyEnv;;
-let env0 = fenv0;;exception EmptyEnv;;
+let rec dval (decls: defin list) = match decls with
+            [ ] -> fenv0
+         |  Funz (fname, par, body)::rest -> ext (dval rest) fname (par, body);;
+let peval (p: prog) = match p with
+            Program (decls, expr) -> let fenv = dval(decls) in eval expr env0 fenv;;
 
-(
-*TEST
-# eval (Int 5) env0 fenv0;;
+
+
+(*
+--- Test Cases ------
+# peval(Program([], (Int 5)));;
 - : int = 5
 *)
