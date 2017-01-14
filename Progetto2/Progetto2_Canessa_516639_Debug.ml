943c2bbf64cc9a4d03c10a8c97f3cbd41a1e98cc
luca <lu995ca@hotmail.it>
2017-01-15 00:25:28 +0100

    non so cosa scrivere

--- Progetto2/Progetto2_Canessa_516639_Debug.ml
+++ Progetto2/Progetto2_Canessa_516639_Debug.ml
@@ -1,81 +1,14 @@
-(* DEBUG FILE *)
-
-
-type binop = Plus
-          | Minus
-          | Mul
-          | Div
-          | Eq
-          | LThan
-          | LEq
-          | GThan
-          | GEq
-;;
-
 type exp =  Int of int
-        |  Ide of string
-        |  App of string * exp
-        |  Op of exp * binop * exp
-        |  If of exp * exp * exp
-        |  Etup of tuple
-        |  Pipe of tuple
-        |  ManyTime of int * exp
+        | ETup of tuple
 and tuple = Nil
           | Seq of exp * tuple
 ;;
 
-type defin = Funz of string * string * exp;;
-
-exception EmptyEnv;;
-let fenv0 = fun x -> raise EmptyEnv;;
-let env0 = fenv0;;
-let ext env e v = fun y -> if e = y then v else env y;;
-
-let rec dval (decls: defin list) = match decls with
-            [ ] -> fenv0
-         |  Funz (fname, par, body)::rest -> ext (dval rest) fname (par, body);;
-let peval (p: prog) = match p with
-     Program (decls, expr) -> let fenv = dval(decls) in eval expr env0 fenv;;
-
-
+type rospo = PInt of exp
+      | Teval of exp * exp
 
 let rec eval (e:exp) env fenv = match e with
-                    Int n -> n
-                  | Ide i -> env i
-                  | App (s, e2) -> let (par,body) = (fenv s)
-                                    in (eval body (ext env par (eval e2 env fenv)) fenv)
-                  | Op (e1, o, e2) -> (
-                                        match o with
-                                          Plus -> (eval e1 env fenv) + (eval e2 env fenv)
-                                        | Minus -> (eval e1 env fenv) - (eval e2 env fenv)
-                                        | Mul -> (eval e1 env fenv) * (eval e2 env fenv)
-                                        | Div -> (eval e1 env fenv) / (eval e2 env fenv)
-                                        | Eq ->  if (eval e1 env fenv) = (eval e2 env fenv) then 1 else 0
-                                        | LThan -> if (eval e1 env fenv) < (eval e2 env fenv) then 1 else 0
-                                        | LEq -> if (eval e1 env fenv) <= (eval e2 env fenv) then 1 else 0
-                                        | GThan -> if (eval e1 env fenv) > (eval e2 env fenv) then 1 else 0
-                                        | GEq -> if (eval e1 env fenv) >= (eval e2 env fenv) then 1 else 0
-                                        | _ -> failwith "wrong operators"
-                                      )
-                  | If (e1,e2,e3) -> if (eval e1 env fenv) = 1
-                                        then (eval e2 env fenv)
-                                          else (eval e3 env fenv)
-
+                | ETup t -> let(x,y) = evalTup t env fenv in Teval (x,y)
 and evalTup (t:tuple) env fenv = match t with
-
-
-                                  | Seq(x, tp) when tp == Nil -> (eval x env fenv)
+             Seq(x,y) -> ((eval x env fenv), (evalTup y env fenv))
 ;;
-
-
-(*
-| ManyTime (n, e) -> let rec foo (n, e) = if (n > 1)
-                                            then foo ((n - 1),(eval e env fenv))
-                                            else (eval e env fenv)
-
-                                            | ManyTime (n, e) -> match n with
-                                                                    x when x > 1 -> (n-1, eval e env fenv)
-                                                                  | x when x = 0 -> (0, eval e env fenv)
-| Etup ex -> evalTup ex env fenv
-| Pipe t -> evalTup t env fenv
-*)
